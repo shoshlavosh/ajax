e06ad1613d44aefbc8d73982453c6705242aeb03
shoshlavosh <shoshlavosh@gmail.com>
2021-08-04 19:53:08 +0000

    parts 1 & 2 of ajax lab completed

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -8,17 +8,27 @@ function showFortune(evt) {
     // TODO: get the fortune and show it in the #fortune-text div
     // $.get('/status', (res) => {
     //     $('#order-status').html(res);
-    $.get()
+    // <div id="fortune-text"></div>
+    //   '/fortune'
+    $.get('/fortune', (res) => {
+        $('#fortune-text').html(res);
+    })
 
 }
 
 $('#get-fortune-button').on('click', showFortune);
 
 
-
+{/* <div id="weather-info"></div> */}
 
 
 // PART 2: SHOW WEATHER
+// WEATHER = {
+//     '94110': {'forecast': 'Rainy, damp, and rich with hipsters.', 'temp': '60F'},
+//     '99507': {'forecast': 'Warm, balmy, and good for sunbathing.', 'temp': '100F'},
+//     '94102': {'forecast': 'Delightful, clever, and full of Python.', 'temp': '55F'},
+// }
+
 
 function showWeather(evt) {
     evt.preventDefault();
@@ -26,6 +36,10 @@ function showWeather(evt) {
     let url = "/weather.json";
     let formData = {"zipcode": $("#zipcode-field").val()};
 
+    $.get(url, formData, (res) => {
+        $('#weather-info').html(res['forecast']);
+        
+    })
 
     // TODO: request weather with that URL and show the forecast in #weather-info
 }
@@ -36,11 +50,15 @@ $("#weather-form").on('submit', showWeather);
 
 
 // PART 3: ORDER MELONS
-
+// /order-melons
+// // $.post()
+// #order-status
+// .addClass() 
 function orderMelons(evt) {
     evt.preventDefault();
 
     // TODO: show the result message after your form
+   
     // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
 
